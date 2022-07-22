e3ba9b8617a05c25dd70a91c0b44b90fb86478b0
Evidentsinger14 <Cdelancey186423@gmail.com>
2022-07-21 20:53:53 -0400

    righto... forgot the check to only cancel spawn eggs for spawners.

--- src/main/java/dev/Ev1dent/HavenSpawners/Events/NoSpawnEggs.java
+++ src/main/java/dev/Ev1dent/HavenSpawners/Events/NoSpawnEggs.java
@@ -1,6 +1,7 @@
 package dev.Ev1dent.HavenSpawners.Events;
 
 import com.destroystokyo.paper.MaterialTags;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,6 +14,8 @@ public class NoSpawnEggs implements Listener {
     @EventHandler
     public void onEggUsage(PlayerInteractEvent event){
         Player player = event.getPlayer();
+        if(event.getClickedBlock() == null) return;
+        if(!event.getClickedBlock().getType().equals(Material.SPAWNER)) return;
         if(MaterialTags.SPAWN_EGGS.isTagged(player.getInventory().getItemInOffHand())
             || MaterialTags.SPAWN_EGGS.isTagged(player.getInventory().getItemInMainHand())){
             event.setCancelled(true);
